9847a8bf904b2c9aacaf8aa88cebb98f2fbad3f7
myunbongs <myunbongs@naver.com>
2020-08-28 17:35:27 +0900

    layout 수정, form 컴포넌트 추가

--- frontend/board-app/src/containers/BoardDetailView.js
+++ frontend/board-app/src/containers/BoardDetailView.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import axios from 'axios';
-import { Card } from 'antd';
+import React from "react";
+import axios from "axios";
+import { Card } from "antd";
+import CustomForm from "../components/Form";
 
 class BoardDetail extends React.Component {
+  state = {
+    board: {},
+  };
 
-    state = {
-        board: {}
-    }
+  componentDidMount() {
+    const boardID = this.props.match.params.boardID;
+    axios.get("http://127.0.0.1:8000/api/" + String(boardID)).then((res) => {
+      this.setState({
+        board: res.data,
+      });
+    });
+  }
 
-    componentDidMount() {
-        const boardID = this.props.match.params.boardID; 
-        axios.get('http://127.0.0.1:8000/api/'+String(boardID))
-            .then(res => {
-                this.setState({
-                    board: res.data
-                });
-            })
-    }
-
-    render() {  
-        return (
-           <Card title={this.state.board.title}>
-               <p>{this.state.board.content}</p>
-           </Card>
-        )
-    }
+  render() {
+    return (
+      <Card title={this.state.board.title}>
+        <p>{this.state.board.content}</p>
+      </Card>
+      //   if (BoardID == UserID) {
+      //       <Card>
+      //       {
+      //       <CustomForm
+      //         requestType="put"
+      //         boardID={this.props.match.params.boardID}
+      //         btnText="수정하기"
+      //       />
+      //       }
+      //       </Card>
+      //   }
+    );
+  }
 }
 
 export default BoardDetail;
--- frontend/board-app/src/containers/BoardListView.js
+++ frontend/board-app/src/containers/BoardListView.js
@@ -1,40 +1,36 @@
-import React from 'react';
-import Board from '../components/Board';
-import axios from 'axios';
+import React from "react";
+import Board from "../components/Board";
+import axios from "axios";
 
 const listData = [];
 for (let i = 0; i < 23; i++) {
   listData.push({
-    href: 'https://ant.design',
+    href: "https://ant.design",
     title: `ant design part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
     description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
     content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
   });
 }
 
 class BoardList extends React.Component {
+  state = {
+    board: [],
+  };
 
-    state = {
-        board: []
-    }
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/api/").then((res) => {
+      this.setState({
+        board: res.data,
+      });
+    });
+  }
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/')
-        .then(res => {
-            this.setState({
-                board: res.data
-            });
-        })
-    }
-
-    render() {  
-        return (
-            <Board data={this.state.board} />
-        )
-    }
+  render() {
+    return <Board data={this.state.board} />;
+  }
 }
 
 export default BoardList;
