9f5d43eb9ba463ec5867de0f1ed2343541f0117c
myunbongs <myunbongs@naver.com>
2020-09-04 17:24:33 +0900

    antd 제거, scss로 디자인

--- frontend/board-app/src/App.js
+++ frontend/board-app/src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import BaseRouter from './routes';
-import 'antd/dist/antd.css';
-import CustomLayOut from './containers/Layout';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import BaseRouter from "./routes";
+import CustomLayOut from "./containers/Layout";
+import CustomForm from "./components/Form";
+import Board from "./components/Board";
 
 class App extends Component {
   render() {
-    return(
-    <div className="App">
-      <Router>
-      <CustomLayOut>
-        <BaseRouter />
-      </CustomLayOut>
-      </Router>
-    </div>
+    return (
+      <div className="App">
+        <Router>
+          <CustomLayOut>
+            <BaseRouter />
+            <CustomForm />
+          </CustomLayOut>
+        </Router>
+      </div>
     );
   }
 }
